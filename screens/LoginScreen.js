0cd3512a6beaaaf3394cb0babfb62cb820ce58fe
claudmiine <wojcieska.klaudia@gmail.com>
2022-02-02 22:19:07 +0000

    Login and Register added

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
+// import all necessary packages, files from react-native
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native'
 
+//load the app Login screens
 const LoginScreen = () => {
     return (
         <KeyboardAvoidingView
@@ -8,32 +10,60 @@ const LoginScreen = () => {
             behavior='padding'
         >
         <View style={styles.inputCointainer}>
+        <Image style ={styles.image}source={require('./logo.png')} /> 
+            <Text>Join us and lets change the world together!</Text>
         <TextInput
+        ////placeholder for email input
         placeholder='Email'
         // value={}
         // onChangeText={text => }
         style={styles.input}
+        //placeholder for password input, text is insvisible
         />
-        
         <TextInput
         placeholder='Password'
         // value={}
         // onChangeText={text => }
         style={styles.input}
+        //that part make sure the password in secured
         secureTextEntry
         />
         </View>
-            <Text>Login Screen</Text>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity
+        onPress={() => { }}
+        style={styles.button}
+        >
+        <Text styles={styles.button}>
+        Login</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity
+        onPress={() => { }}
+        style={styles.button, styles.buttonOutline}
+        >
+        <Text styles={styles.buttonOutlineText}>
+        Register</Text>
+        </TouchableOpacity>
+        </View>
         </KeyboardAvoidingView>
+        
     )
 }
 
+//export file to access from outside
 export default LoginScreen
 
+// create style
 const styles = StyleSheet.create({
 container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems:'center',
+    textAlign: 'center', // <-- the magic
+    fontWeight: 'bold',
+    },
+image:{
     alignItems:'center',
     }
 })
